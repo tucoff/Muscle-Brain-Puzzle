c15a745420fd70385c2054beeb115fe1493d5eb7
Artur Raffael <48564033+tucoff@users.noreply.github.com>
2024-08-22 13:26:43 -0300

    Update InteractiveObject.cs

--- Assets/Scripts/InteractiveObject.cs
+++ Assets/Scripts/InteractiveObject.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using Utils;
 
 public class InteractiveObject : MonoBehaviour
@@ -13,38 +12,24 @@ public class InteractiveObject : MonoBehaviour
 
     public void Interact(Directions direction)
     {
-        if(elementNeeded == GameObject.Find("Player").GetComponent<PlayerBehaviour>().inUseElement)
+        if (elementNeeded == GameObject.Find("Player").GetComponent<PlayerBehaviour>().inUseElement)
         {
             //Só funciona no código da porta, precisa ser modificado posteriormente
-            objectToInteract.SetActive(false);
+            if (objectToInteract != null)
+            {
+                objectToInteract.SetActive(false);
+            }
         }
 
-        if(isQuebravel && GameObject.Find("Player").GetComponent<PlayerBehaviour>().luvas && GameObject.Find("Player").GetComponent<PlayerBehaviour>().inUseElement == Element.Air)
+        if (isQuebravel && GameObject.Find("Player").GetComponent<PlayerBehaviour>().luvas && GameObject.Find("Player").GetComponent<PlayerBehaviour>().inUseElement == Element.Air)
         {
             Destroy(this.gameObject);
-            switch (direction)
-            {
-                case Directions.North:
-                    GameObject.Find("North").GetComponent<IsColliding>().isColliding = false;
-                    break;
-                case Directions.South:
-                    GameObject.Find("South").GetComponent<IsColliding>().isColliding = false;
-                    break;
-                case Directions.East:
-                    GameObject.Find("East").GetComponent<IsColliding>().isColliding = false;
-                    break;
-                case Directions.West:
-                    GameObject.Find("West").GetComponent<IsColliding>().isColliding = false;
-                    break;
-            }
-            GameObject.Find("North").GetComponent<IsColliding>().ChangeDirection();
         }
-        else if(isLuva)
+        else if (isLuva)
         {
             GameObject.Find("Player").GetComponent<PlayerBehaviour>().luvas = true;
             isLuva = false;
             GameObject.Find("LuvasLoucas").SetActive(false);
-            GameObject.FindWithTag("CurrentElement").GetComponent<Image>().color = Color.white;
         }
         else
         {
